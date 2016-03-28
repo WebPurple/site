62a39aa4616c886271f0c54a67a50680e3d969fa
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-03-28 21:32:54 +0300

    if user don't have vkPhotoUrl, user icon is displayed now

--- components/react/app.header.tsx
+++ components/react/app.header.tsx
@@ -4,6 +4,7 @@ import {connect} from 'react-redux';
 
 import * as AppBar from 'material-ui/lib/app-bar';
 import * as Avatar from 'material-ui/lib/avatar';
+import * as SocialPerson from "material-ui/lib/svg-icons/social/person";
 import * as IconButton from "material-ui/lib/icon-button";
 import * as Menu from "material-ui/lib/svg-icons/navigation/menu";
 import * as NavigationClose from "material-ui/lib/svg-icons/navigation/close";
@@ -16,15 +17,20 @@ function getUserName(user) {
     return user.vkDisplayName || user.fbDisplayName;
 }
 
+const AppAvatar = ({user}) => (
+    user.vkPhotoUrl ? <Avatar src={user.vkPhotoUrl}/>
+        : <Avatar icon={<SocialPerson/>}/>
+);
+
 const AppHeaderComponent = ({user, onToggleLeftNav, leftNavOpen}) => (
     <AppBar title={'WebPurple' + (user ? ` | ${getUserName(user)}` : '')}
             iconElementLeft={<IconButton onTouchTap={onToggleLeftNav}>{leftNavOpen ? <NavigationClose /> : <Menu/>}</IconButton>}
-            iconElementRight={user ? <Avatar src={user.vkPhotoUrl}/>: <LoginComponent />}/>
+            iconElementRight={user ? <AppAvatar user={user}/> : <LoginComponent />}/>
 );
 
 const AppHeaderContainer = connect(
     state => Object.assign({}, state.header, {leftNavOpen: state.leftNav.leftNavOpen}),
-    (dispatch: Redux.Dispatch) => {
+    (dispatch:Redux.Dispatch) => {
         return {
             onToggleLeftNav: () => dispatch(toggleLeftNav())
         }
