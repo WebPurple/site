db2d4a6b8cce48cc8438c51bf25103d4fb2173b9
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-05-06 16:47:05 +0300

    singleRun always true now

--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function (config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: process.env.NODE_ENV === 'production',
+        singleRun: true,
 
         // Concurrency level
         // how many browser should be started simultaneous
