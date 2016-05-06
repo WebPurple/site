1c117b4310c8ca9cd1878d6c591270c3d34bcb79
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-05-06 16:23:53 +0300

    CI tests run mode fixed

--- karma.conf.js
+++ karma.conf.js
@@ -66,7 +66,7 @@ module.exports = function (config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false,
+        singleRun: process.env.NODE_ENV === 'production',
 
         // Concurrency level
         // how many browser should be started simultaneous
