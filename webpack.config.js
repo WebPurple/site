0f119b061e50e24e91cd94a6a258028affc87ee0
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-08-11 14:20:17 +0300

    one more try to fix "ERROR in Entry module not found: Error: Cannot resolve module 'babel'"

--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
             {
                 test: /\.jsx?$/,
                 exclude: /node_modules/,
-                loader: 'babel',
+                loader: 'babel-loader',
                 query: {
                     presets: ['es2015', 'react'],
                     plugins: ['transform-object-rest-spread']
