d59ab0c92365c4d278fb51596d69794a4f0e8f70
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-08-11 15:10:35 +0300

    VK_CALLBACK_URL env property was added

--- src/server/conf/passport.js
+++ src/server/conf/passport.js
@@ -5,7 +5,7 @@ module.exports = {
         vk: {
             clientID: '5360165',
             clientSecret: '765utMWmnVn9CS1kOuLB',
-            callbackURL: `${appConf.protocol}://${appConf.host}:${appConf.port}/auth/vk/callback`
+            callbackURL: process.env.VK_CALLBACK_URL || `${appConf.protocol}://${appConf.host}:${appConf.port}/auth/vk/callback`
         },
 
         fb: {
