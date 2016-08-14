afd5a565f1d7a10eb086e4bd16cbc93749393043
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-08-15 01:19:55 +0300

    test for user.reducer was implemented

--- test/reducers/user.reducer.test.js
+++ test/reducers/user.reducer.test.js
@@ -1,7 +1,41 @@
 import { expect } from 'chai';
 
 import userReducer from './../../src/reducers/user.reducer';
+import {
+    requestUser,
+    receiveUser,
+    changeUserName,
+    SAVE_USER,
+} from './../../src/actions/user.actions';
 
 describe('user.reducer', () => {
-    it('should exists', () => expect(userReducer).to.be.defined);
+    const state = { account: {} };
+    Object.freeze(state);
+
+    it('should create empty object on init', () => {
+        expect(userReducer(undefined, {})).to.be.defined;
+    });
+
+    it('should return same state by default', () => {
+        expect(userReducer(state, {})).to.equal(state);
+    });
+
+    it('should set isFetching to true on REQUEST_USER or SAVE_USER', () => {
+        expect(userReducer(state, { type: SAVE_USER }).isFetching).to.be.true;
+        expect(userReducer(state, requestUser()).isFetching).to.be.true;
+    });
+
+    it('should set isFetching to false on RECEIVE_USER', () => {
+        expect(userReducer(state, receiveUser({})).isFetching).to.be.false;
+    });
+
+    it('should save received user', () => {
+        const user = 'user1';
+        expect(userReducer(state, receiveUser(user)).account).to.equal(user);
+    });
+
+    it('should update user name on CHANGE_USER_NAME', () => {
+        const userName = 'userName';
+        expect(userReducer(state, changeUserName(userName)).account.displayName).to.equal(userName);
+    });
 });
