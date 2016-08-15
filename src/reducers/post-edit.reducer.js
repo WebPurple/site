cd478479be77cbd2fca84c251da2d811a25d350c
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-08-15 15:54:34 +0300

    post-edit.reducer was refactored (object spread operator is used)

--- src/reducers/post-edit.reducer.js
+++ src/reducers/post-edit.reducer.js
@@ -12,31 +12,61 @@ import {
 const editPost = (state = { post: { text: '' }, deferredPost: false }, action) => {
     switch (action.type) {
         case CHANGE_POST_LINK:
-            return Object.assign({}, state, {
-                post: Object.assign({}, state.post, { link: action.payload }),
-            });
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    link: action.payload,
+                },
+            };
         case CHANGE_POST_TEXT:
-            return Object.assign({}, state, {
-                post: Object.assign({}, state.post, { text: action.payload }),
-            });
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    text: action.payload,
+                },
+            };
         case CHANGE_POST_LINK_TITLE:
-            return Object.assign({}, state, {
-                post: Object.assign({}, state.post, { linkTitle: action.payload }),
-            });
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    linkTitle: action.payload,
+                },
+            };
         case CHANGE_POST_IMAGE:
-            return Object.assign({}, state, {
-                post: Object.assign({}, state.post, { imageLink: action.payload }),
-            });
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    imageLink: action.payload,
+                },
+            };
         case DEFERRED_POST:
-            return Object.assign({}, state, { deferredPost: !state.deferredPost });
+            return {
+                ...state,
+                deferredPost: !state.deferredPost,
+            };
         case EXPORT_TO_FACEBOOK:
-            return Object.assign({}, state, {
-                post: Object.assign({}, state.post, { exportToFacebook: action.payload }),
-            });
+            return {
+                ...state,
+                post: {
+                    ...state.post,
+                    exportToFacebook: action.payload,
+                },
+            };
         case SUBMIT_POST_FORM:
-            return Object.assign({}, state, { isFetching: true });
+            return {
+                ...state,
+                isFetching: true,
+            };
         case POST_ADDED:
-            return Object.assign({}, state, { post: { text: '' }, isFetching: false });
+            return {
+                ...state,
+                post: { text: '' },
+                isFetching: false,
+            };
         default:
             return state;
     }
