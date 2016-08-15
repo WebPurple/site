bb4ebeb271266712853706d626299f4b7a0c5726
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-08-15 14:58:02 +0300

    test for users-page.reducer was implemented

--- test/reducers/users-page.reducer.test.js
+++ test/reducers/users-page.reducer.test.js
@@ -1,7 +1,20 @@
 import { expect } from 'chai';
 
 import usersPageReducer from './../../src/reducers/users-page.reducer';
+import { receiveAllUsers } from './../../src/actions/user.actions';
 
 describe('users-page.reducer', () => {
-    it('should exists', () => expect(usersPageReducer).to.be.defined);
+    const state = Object.freeze({});
+
+    it('should create empty object on init', () => expect(usersPageReducer(undefined, {})).to.be.defined);
+
+    it('should return old state on unknown action', () => expect(usersPageReducer(state, {})).to.equal(state));
+
+    describe('RECEIVE_ALL_USERS action', () => {
+        const users = 'users';
+
+        it('should save received users', () => {
+            expect(usersPageReducer(state, receiveAllUsers(users)).users).to.equal(users);
+        });
+    });
 });
