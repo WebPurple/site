1b4c34827f866d9dfbc60ff65871f4fbe5080c2a
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-09-11 11:16:14 +0300

    resolve #21 - vk community widget was added

--- src/components/about-page.jsx
+++ src/components/about-page.jsx
@@ -2,12 +2,17 @@ import * as React from 'react';
 
 import { Card, CardTitle, CardText } from 'material-ui/Card';
 
+import VkWidget from './vk-community-widget';
+
 const AboutPage = () => (
     <div>
         <Card>
             <CardTitle title="Webpurple" />
             <CardText>Webpurple is a Ryazan front-end community...</CardText>
         </Card>
+        <div style={{ paddingTop: 20 }}>
+            <VkWidget />
+        </div>
     </div>
 );
 
