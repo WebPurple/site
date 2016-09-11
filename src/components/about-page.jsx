aabafd29bdc226b1b5e8d92646204d1c03340a1a
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-09-11 15:33:44 +0300

    github button was added

--- src/components/about-page.jsx
+++ src/components/about-page.jsx
@@ -4,12 +4,22 @@ import { Card, CardTitle, CardText } from 'material-ui/Card';
 
 import VkWidget from './social/vk-community-widget';
 import FbWidget from './social/fb-community-widget';
+import GithubButton from './social/github-button';
 
 const AboutPage = () => (
     <div>
         <Card>
             <CardTitle title="Webpurple" />
-            <CardText>Webpurple is a Ryazan front-end community...</CardText>
+            <CardText>
+                <p>
+                    Webpurple is a Ryazan front-end community...
+                </p>
+                <p>
+                    By the way this site is open sourse project, so feel free to contribute
+                    (and don't forget to add start to a project if you like it).
+                </p>
+                <GithubButton />
+            </CardText>
         </Card>
         <ul style={{ display: 'flex', justifyContent: 'space-around', listStyle: 'none', padding: 0, paddingTop: 20 }}>
             <li><VkWidget height={170} width={400} /></li>
