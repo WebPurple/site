ceb1034b0880fcc65a7d2dda27392a8fe5112f3c
Tri Tran <tvtri96@yahoo.com.vn>
2016-10-19 17:52:14 +0300

    Fix global require error

--- src/server/server.js
+++ src/server/server.js
@@ -20,6 +20,7 @@ const pageInfoApi = require('./controllers/page-info.controller');
 const webpack = require('webpack');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const webpackMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
 const webpackConf = require('../../webpack.config');
 
 const isProd = process.env.NODE_ENV === 'production';
@@ -47,7 +48,7 @@ if (isProd) {
 } else {
     const compiler = webpack(webpackConf);
     app.use(webpackMiddleware(compiler, { publicPath: webpackConf.output.publicPath }));
-    app.use(require("webpack-hot-middleware")(compiler));
+    app.use(webpackHotMiddleware(compiler));
 }
 
 app.use(express.static('favicon'));
