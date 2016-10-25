520568b4c7f165e405502c4f1f2b3900f81df943
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-10-25 22:02:09 +0300

    :rotating_light: async test for feed.actions

--- test/containers/feed/feed.actions.test.js
+++ test/containers/feed/feed.actions.test.js
@@ -1,6 +1,12 @@
 import { expect } from 'chai';
+import fetchMock from 'fetch-mock';
+import configureMockStore from 'redux-mock-store';
+
+import thunk from 'redux-thunk';
 
 import * as actions from '../../../src/containers/feed/feed.actions';
+
+const mockStore = configureMockStore([thunk]);
 
 describe('feed.actions', () => {
     it('should create an action to remove post', () => {
@@ -12,19 +18,52 @@ describe('feed.actions', () => {
             });
     });
 
-    it('should create and action to request posts', () => {
+    it('should create an action to request posts', () => {
         expect(actions.requestPosts())
             .to.deep.equal({
                 type: actions.REQUEST_POSTS,
             });
     });
 
-    it('should create and action to receive posts', () => {
+    it('should create an action to receive posts', () => {
         const posts = [{ _id: 1 }];
         expect(actions.receivePosts(posts))
             .to.deep.equal({
                 type: actions.RECEIVE_POSTS,
                 payload: posts,
             });
+    });
+
+    describe('async', () => {
+        it('should create an action to fetch posts', () => {
+            const posts = [{ _id: 1 }];
+            const store = mockStore();
+
+            fetchMock.get('/api/posts', posts);
+
+            store.dispatch(actions.fetchPosts())
+                .then(() => {
+                    expect(fetchMock.called('/api/posts')).to.be.true;
+                    expect(store.getActions()).to.deep.equal([
+                        { type: actions.REQUEST_POSTS },
+                        { type: actions.RECEIVE_POSTS, payload: posts },
+                    ]);
+                });
+        });
+
+        it('should create an action to delete posts', () => {
+            const post = { _id: 1 };
+            const store = mockStore();
+
+            fetchMock.delete('/api/posts/1', post);
+
+            store.dispatch(actions.deletePost(1))
+                .then(() => {
+                    expect(fetchMock.called('/api/posts/1')).to.be.true;
+                    expect(store.getActions()).to.deep.equal([
+                        { type: actions.POST_REMOVED, payload: post },
+                    ]);
+                });
+        });
     });
 });
