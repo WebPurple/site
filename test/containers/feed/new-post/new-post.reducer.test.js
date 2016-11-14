40763526c324656e78b1dc4a64c9276f8dae8c55
Nikita Kirsanov <nikita_kirsanov@epam.com>
2016-11-14 10:58:21 +0300

    :rotating_light: tests for new-post.reducer

--- test/containers/feed/new-post/new-post.reducer.test.js
+++ test/containers/feed/new-post/new-post.reducer.test.js
@@ -2,7 +2,12 @@ import { expect } from 'chai';
 import deepFreeze from 'deep-freeze';
 
 import newPostReducer from '../../../../src/containers/feed/new-post/new-post.reducer';
-import { openDialog, closeDialog } from '../../../../src/containers/feed/new-post/new-post.actions';
+import {
+    openDialog,
+    closeDialog,
+    closeErrorDialog,
+    editPost,
+} from '../../../../src/containers/feed/new-post/new-post.actions';
 import { postAdded } from '../../../../src/containers/feed/feed.actions';
 
 describe('new-post.reducer', () => {
@@ -18,4 +23,15 @@ describe('new-post.reducer', () => {
 
     it('should close dialog when post is saved',
         () => expect(newPostReducer(state, postAdded()).state.dialogOpen).to.be.false);
+
+    it('should show error when post addition failed', () => {
+        const error = 'error';
+        expect(newPostReducer(state, postAdded(error, true)).state.error).to.equal(error);
+    });
+
+    it('should close error',
+        () => expect(newPostReducer(state, closeErrorDialog()).state.error).to.not.exist);
+
+    it('should open dialog on post edit',
+        () => expect(newPostReducer(state, editPost()).state.dialogOpen).to.be.true);
 });
