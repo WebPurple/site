d995b528735b317c5024ef202ff3a34f50728008
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-02-07 16:32:52 +0300

    :lipstick: add stubs for home page blocks

--- src/components/home-page/home-page.jsx
+++ src/components/home-page/home-page.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { media } from '../../utils/css-utils';
+
+import SubscriptionForm from './subscription-block';
+
+const EmptyBlock = styled.section`
+    font-family: Rubik;
+    font-size: 48px;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 100px;
+    ${media.tablet`line-height: 200px;`}
+    ${media.hd`line-height: 300px;`}
+`;
 
 const HomePage = () => (
     <div>
-        <section>Upcoming events</section>
-        <section>Passed events</section>
-        <section>Subscription block</section>
-        <section>Social networks block</section>
+        <EmptyBlock>Upcoming events</EmptyBlock>
+        <EmptyBlock>Passed events</EmptyBlock>
+        <SubscriptionForm />
+        <EmptyBlock>Social networks block</EmptyBlock>
     </div>
 );
 
