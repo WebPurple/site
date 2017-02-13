3894a6701d851830a9831561247aceb2b3ecce10
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-02-13 12:34:56 +0300

    change calculation in media queries, cause body font-size is 10 (index.html)

--- src/utils/css-utils.js
+++ src/utils/css-utils.js
@@ -11,7 +11,7 @@ const sizes = {
 export const media = Object.keys(sizes).reduce((accumulator, label) => {
     // use em in breakpoints to work properly cross-browser and support users
     // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
-    const emSize = sizes[label] / 16;
+    const emSize = sizes[label] / 10;
 
     // eslint-disable-next-line no-param-reassign
     accumulator[label] = (...args) => css`
