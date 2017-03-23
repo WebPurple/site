68bb60b5b14bb1f6757d0c2cb5fb5ffa7f3eab61
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-03-23 21:49:05 +0300

    load pages asynchronously

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,40 @@
-import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-
 import App from './containers/app/app';
-import HomePage from './components/home-page/home-page';
 
-export default (
-    <Route path="/" component={App}>
-        <IndexRoute component={HomePage} />
-        <Route path="home" component={HomePage} />
-    </Route>
-);
+const loadModule = cb => module => cb(null, module.default);
+
+const handleError = err => console.error(`Failed to load route component: ${err}`);
+
+export default {
+
+    path: '/',
+
+    component: App,
+
+    getIndexRoute(partialNextState, cb) {
+        import('./components/home-page/home-page')
+            .then(module => cb(null, { component: module.default }))
+            .catch(handleError)
+    },
+
+    childRoutes: [
+        {
+            path: 'home',
+
+            getComponents(nextState, cb) {
+                import('./components/home-page/home-page')
+                    .then(loadModule(cb))
+                    .catch(handleError)
+            },
+        },
+        {
+            path: 'events',
+
+            getComponents(nextState, cb) {
+                import('./components/events-page/events-page')
+                    .then(loadModule(cb))
+                    .catch(handleError)
+            },
+        },
+    ],
+
+};
