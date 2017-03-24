d8c937de240f331fd6d7e25557d2d95b4b139e99
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-03-24 15:17:44 +0300

    add events feed component

---
+++ src/components/events-page/events-feed.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import BlockHeader from '../common/block-header';
+
+const EventsFeed = () => (
+    <section>
+        <BlockHeader>Events</BlockHeader>
+    </section>
+);
+
+export default EventsFeed;
