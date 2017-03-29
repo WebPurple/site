e6bdd76a37f7f60969982ac596c21157d60fe109
Egor_Leontev <egor.leontev@kronos.com>
2017-03-29 13:19:10 +0300

    add flex vertical property

--- src/components/common/filter-block.js
+++ src/components/common/filter-block.js
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 const FilterBlock = styled.div`
     display: flex;
-    justify-content: space-between;
     margin: 4rem 0;
+    align-items: stretch;
+    justify-content: space-between;
     border-bottom: 2px solid #ededed;
+    height: 8.5rem;
 `;
 
 export default FilterBlock;
