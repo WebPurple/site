3022a0cee1aa7e8f6ea4371c66ecc5c599cc2c0e
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-04-08 23:34:00 +0300

    :sparkles: add contributors page

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,6 +2,7 @@ import App from './containers/app/app';
 import HomePage from './components/home-page/home-page';
 import EventsPage from './containers/events/events-page';
 import SpeakersPage from './components/speakers-page/speakers-page';
+import ContributorsPage from './containers/contributors/contributors-page';
 
 export default [{
     component: App,
@@ -23,6 +24,10 @@ export default [{
         {
             path: '/speakers',
             component: SpeakersPage,
+        },
+        {
+            path: '/contributors',
+            component: ContributorsPage,
         },
     ],
 }];
