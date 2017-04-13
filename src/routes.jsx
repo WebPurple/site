aedc3537cc14246236920b9a772be988f80af0be
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-04-14 00:22:25 +0300

    :sparkles: upgrade react-router - index route works fine now in renderRoutes

--- src/routes.jsx
+++ src/routes.jsx
@@ -8,11 +8,11 @@ export default [{
     component: App,
 
     routes: [
-// waiting for https://github.com/ReactTraining/react-router/commit/a4b8408cff1f18541f17066dfe90756ed656fb04
-//        { path: '/',
-//            exact: true,
-//            component: HomePage,
-//        },
+        {
+            path: '/',
+            exact: true,
+            component: HomePage,
+        },
         {
             path: '/home',
             component: HomePage,
