ad16c99b9a30cc2ac1b96b0dd05a1509ad28baa5
Dmitry Rykov <dmitry_rykov@epam.com>
2017-04-14 17:01:18 +0300

    Update Event and User schemas

--- src/server/schemas/event.schema.js
+++ src/server/schemas/event.schema.js
@@ -2,17 +2,26 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const talkFormat = {
+    title: { type: String, default: null },
+    speaker: { type: Schema.Types.ObjectId, ref: 'user', default: null },
+    description: { type: String, default: null },
+    links: {
+        video: { type: String, default: null },
+        presentation: { type: String, default: null },
+    },
+};
+
 const eventSchema = Schema({ // eslint-disable-line new-cap
-    title: String,
-    description: String,
-    image: String,
-    date: Date,
-    location: String,
-    talks: [{
-        title: String,
-        speaker: { type: Schema.Types.ObjectId, ref: 'user' },
-    }],
-    tags: [String],
+    title: { type: String, default: null },
+    description: { type: String, default: null },
+    image: { type: String, default: null },
+    date: { type: Date, default: Date.now() },
+    location: { type: String, default: null },
+    talks: { type: [talkFormat], default: [] },
+    tags: { type: [String], default: [] },
+    attendees: { type: [{ type: Schema.Types.ObjectId, ref: 'user' }], default: [] },
+    vkId: { type: Number, default: null },
 });
 
 module.exports = eventSchema;
