2d2cbc5d7865068a973ec2ecc4d3d5a559fc25ec
Boris Abdulov <boris_abdulov@epam.com>
2017-04-14 13:26:53 +0300

    Add popup component

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,3 +2,4 @@ export * from './social';
 export { default as SearchIcon } from './search-icon';
 export { default as PlaceholderIcon } from './placeholder-icon';
 export { default as ClockIcon } from './clock-icon';
+export { default as CloseIcon } from './close-icon';
