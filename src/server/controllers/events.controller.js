fe86d63d697b66845a06e9c1d4d21ed323d69ff0
Dmitry Rykov <dmitry_rykov@epam.com>
2017-04-14 17:03:08 +0300

    Add back-end actions for event page

--- src/server/controllers/events.controller.js
+++ src/server/controllers/events.controller.js
@@ -2,6 +2,14 @@ const express = require('express');
 
 const eventsService = require('../services/events.service');
 
+function isAuthenticated(request, response, next) {
+    if (request.isAuthenticated()) {
+        next();
+    } else {
+        response.status(401).send('Not authorized');
+    }
+}
+
 module.exports = () => {
     const router = express.Router(); // eslint-disable-line new-cap
 
@@ -10,6 +18,26 @@ module.exports = () => {
         .get((request, response) => eventsService.getEvents()
             .then(post => response.send(post))
             .catch(err => response.send(err)));
+
+    router.route('/event/:eventId')
+        .get((request, response) => eventsService.getEventWithAttendeesAndTalks(request.params.eventId)
+            .then(event => response.send(event))
+            .catch(err => response.code(500).send(err)));
+
+    router.route('/event/:eventId/attendees')
+        // add curret user as attendee to event
+        .put(isAuthenticated, (request, response) => eventsService.addAttendeeToEvent(request.params.eventId, request.user._id)
+            .then((event) => response.send(event))
+            .catch((error) => response.send(error)))
+        // remove current user as attendee from event
+        .delete(isAuthenticated, (request, response) => eventsService.removeAttendeeFromEvent(request.params.eventId, request.user._id)
+            .then((event) => response.send(event))
+            .catch((error) => response.send(error)));
+
+    router.route('/event/:eventId/photos')
+        .get((request, response) => eventsService.getEventPhotos(request.params.eventId)
+            .then(photos => response.send(photos))
+            .catch(() => response.send([])));
 
     return router;
 };
--- src/server/services/events.service.js
+++ src/server/services/events.service.js
@@ -1,14 +1,55 @@
 const mongoose = require('mongoose');
 const eventSchema = require('./../schemas/event.schema');
+const VkGroupId = require('./../conf/passport').strategies.vk.groupId;
 
 const Event = mongoose.model('event', eventSchema);
 
 function getEvents() {
     return Event.find()
         .populate('talks.speaker')
+        .select({ attendees: 0, vkId: 0 })
         .exec();
+}
+
+function getEventWithAttendeesAndTalks(eventId) {
+    return Event.findById(eventId)
+        .select({ vkId: 0 })
+        .populate('attendees', 'displayName vkPhotoUrl')
+        .populate('talks.speaker', 'displayName vkPhotoUrl jobTitle')
+        .exec();
+}
+
+function addAttendeeToEvent(eventId, attendeeId) {
+    return Event.findById(eventId).attendees.push(new mongoose.Types.ObjectId(attendeeId)).exec();
+}
+
+function removeAttendeeFromEvent(eventId, attendeeId) {
+    return Event.findById(eventId).attendees.pull(new mongoose.Types.ObjectId(attendeeId)).exec();
+}
+
+function getEventPhotos(eventId) {
+    return Event.findById(eventId)
+        .select({ vkId: 1 })
+        .exec()
+        .then(event => {
+            if (!event.vkId) {
+                return [];
+            }
+            return fetch(`https://api.vk.com/method/photos.get?owner_id=${VkGroupId}&album_id=${event.vkId}&v=5.63`, {
+                method: 'GET',
+            })
+                .then(response => response.status === 200 ? response.json() : Promise.reject(response.statusText))
+                .then(data => data.response.items)
+                .then(images => images.map(imageObject => imageObject.photo_604))
+                .catch(() => []);
+        })
+        .catch(() => []);
 }
 
 module.exports = {
     getEvents,
+    addAttendeeToEvent,
+    removeAttendeeFromEvent,
+    getEventWithAttendeesAndTalks,
+    getEventPhotos,
 };
