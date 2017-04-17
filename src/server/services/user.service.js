8e812f7059c06b6b366e13d14b173211b30f7633
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-04-18 00:04:59 +0300

    :recycle: move communication with db to service

--- src/server/services/user.service.js
+++ src/server/services/user.service.js
@@ -3,10 +3,32 @@ const userSchema = require('./../schemas/user.schema');
 
 const User = mongoose.model('users', userSchema);
 
-function createUser(user) {
-    return new User(user).save();
-}
+const createUser = user => (new User(user)).save();
+
+const getUser = id => User.findById(id).lean().exec();
+
+const getUsers = () => User.find().lean().exec();
+
+const updateUser = (id, user) => User.findById(id).exec()
+    .then(userToUpdate => {
+        const { displayName, email, roles } = user;
+        /* eslint-disable no-param-reassign */
+        if (displayName) {
+            userToUpdate.displayName = displayName;
+        }
+        if (email) {
+            userToUpdate.email = email;
+        }
+        if (roles) {
+            userToUpdate.roles = roles;
+        }
+        /* eslint-enable no-param-reassign */
+        return userToUpdate.save();
+    })
 
 module.exports = {
     createUser,
+    getUser,
+    updateUser,
+    getUsers,
 };
