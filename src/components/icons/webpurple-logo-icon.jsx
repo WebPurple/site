7a9431c78a3e9603398c82874dd93b51eafb4aab
Egor_Leontev <egor.leontev@kronos.com>
2017-04-18 12:07:05 +0300

    remove id from logoComponent, replace boolean props to color (logo-component is more flexible now), move footer from app.js to new component

--- src/components/icons/webpurple-logo-icon.jsx
+++ src/components/icons/webpurple-logo-icon.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-const WebpurpleLogoIcon = ({ isGray }) => (
+const WebpurpleLogoIcon = ({ color }) => (
     <svg
         width="35px"
         height="31px"
@@ -29,20 +29,20 @@ const WebpurpleLogoIcon = ({ isGray }) => (
                 <g transform="translate(516.000000, 169.000000)">
                     <g transform="translate(96.000000, 40.000000)">
                         <g>
-                            <g transform="translate(-0.000000, 0.206061)" id="logo-icon">
+                            <g transform="translate(-0.000000, 0.206061)">
                                 <polygon
-                                    fill={isGray ? '#b3a8c1' : 'url(#linearGradient-1)'}
+                                    fill={color || 'url(#linearGradient-1)'}
                                     points="25.884174 30.7265263 25.9148433 0.0504544753 34.522761 5.15369572 34.4920916 25.6232851" />
                                 <polygon
-                                    fill={isGray ? '#b3a8c1' : 'url(#linearGradient-2)'}
+                                    fill={color || 'url(#linearGradient-2)'}
                                     points="0.0543461312 25.6232851 8.66226382 30.7265263 17.2762563 25.6232851 25.884174 30.7265263 34.4920916 25.6232851 17.2762563 15.4107988" />
                                 <polygon
-                                    fill={isGray ? '#b3a8c1' : 'url(#linearGradient-3)'}
-                                    opacity={isGray ? 1 : 0.4}
-                                    style={{ mixBlendMode: isGray ? 'normal' : 'multiply' }}
+                                    fill={color || 'url(#linearGradient-3)'}
+                                    opacity={color ? 1 : 0.4}
+                                    style={{ mixBlendMode: color ? 'normal' : 'multiply' }}
                                     points="8.66226382 30.7265263 17.2762563 15.4107988 0.0543461312 25.6232851" />
                                 <polygon
-                                    fill={isGray ? '#b3a8c1' : 'url(#linearGradient-4)'}
+                                    fill={color || 'url(#linearGradient-4)'}
                                     points="0.0543461312 25.6232851 0.150496129 5.15369624 8.75841382 0.0504549893 8.66226382 30.7265263" />
                             </g>
                         </g>
--- src/containers/app/app.jsx
+++ src/containers/app/app.jsx
@@ -7,7 +7,7 @@ import { renderRoutes } from 'react-router-config';
 import { media } from '../../utils/css-utils';
 
 import LoginPopup from '../../components/login/login-popup';
-import WebpurpleLogoIcon from '../../components/icons/webpurple-logo-icon';
+import FooterComponent from '../../components/footer';
 
 const Container = styled.div`
     margin: 0 auto;
@@ -63,65 +63,6 @@ const NavigationLink = styled(NavLink)`
     }
 `;
 
-const Footer = styled.footer`
-    bottom: 0;
-    background-color: ${props => props.theme.grape};
-    box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.3);
-    padding: 3rem 2rem 3.5rem;
-    ${media.tablet`
-        display: flex;
-        padding: 4rem 2rem 3rem;
-        align-items: center;
-        justify-content: space-between;
-    `}
-    ${media.desktop`padding: 4rem 10rem 3rem;`}
-`;
-
-const FooterElementsStyle = styled.p`
-    margin: 0;
-    color: #fff;
-    font-family: 'Oxygen', sans-serif;
-    font-size: 1.6rem;
-    font-weight: bold;
-    text-align: center;
-    ${media.phone`
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        text-align: left;
-    `}
-    ${media.tablet`font-size: 1.8rem;`}
-`;
-
-const BlockElem = styled.p`
-    margin: 1rem 0;
-    text-align: center;
-    ${media.phone`text-align: left;`}
-`;
-
-const LogoContainer = styled(BlockElem)`
-    ${media.phone`padding-right: 0.8rem;`}
-    
-`;
-
-const Copyrights = styled(FooterElementsStyle)`
-    opacity: 0.6;
-    ${media.tablet`margin-right: auto;`}
-`;
-
-const Contacts = styled(FooterElementsStyle)`
-    ${media.tablet`margin-left: auto;`}
-`;
-
-const MailInfo = styled.a`
-    color: ${props => props.theme.lipstick};
-    text-decoration: none;
-    
-    &:hover {
-        text-decoration: underline;
-    }
-`;
-
 const theme = {
     grape: '#432867',
     warmGrey: '#a1a1a1',
@@ -150,14 +91,7 @@ const AppContainer = ({ route }) => (
             <main>
                 {renderRoutes(route.routes)}
             </main>
-            <Footer>
-                <Copyrights>
-                    <LogoContainer>
-                        <WebpurpleLogoIcon isGray />
-                    </LogoContainer><BlockElem>&#169; 2013 - {new Date().getFullYear()} Webpurple.</BlockElem><BlockElem>All rights reserved.</BlockElem>
-                </Copyrights>
-                <Contacts>Contact us <MailInfo href="mailto:info@webpurle.com">info@webpurle.com</MailInfo></Contacts>
-            </Footer>
+            <FooterComponent />
         </Container>
     </ThemeProvider>
 );
