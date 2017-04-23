3a0b05349b1798f907608ee3ae053defeca643b6
Nikita Kirsanov <nikita_kirsanov@epam.com>
2017-04-23 19:08:15 +0300

    :recycle: move user actions to reducer, use redux-actions package to reduce boilerplate

--- src/boot.jsx
+++ src/boot.jsx
@@ -7,7 +7,7 @@ import { AppContainer } from 'react-hot-loader';
 
 import './polyfills';
 
-import { fetchUser } from './actions/user.actions';
+import { fetchUser } from './reducers/user.reducer';
 
 import configureStore from './configureStore';
 import Root from './components/root';
--- src/reducers/user.reducer.js
+++ src/reducers/user.reducer.js
@@ -1,11 +1,21 @@
-import {
-    RECEIVE_USER,
-    SAVE_USER,
-    REQUEST_USER,
-    RECEIVE_ROLES,
-} from '../actions/user.actions';
+import { createAction } from 'redux-actions';
 
-const userReducer = (state = {}, action) => {
+import {
+    getJson,
+    putJson,
+} from '../utils/ajax';
+
+export const REQUEST_USER = 'request_user';
+export const RECEIVE_USER = 'receive_user';
+export const SAVE_USER = 'save_user';
+export const REQUEST_ALL_USERS = 'request_all_users';
+export const RECEIVE_ALL_USERS = 'receive_all_users';
+export const REQUEST_ROLES = 'request_roles';
+export const RECEIVE_ROLES = 'receive_roles';
+export const ROLE_ADDED = 'role_added';
+export const ROLE_REMOVED = 'role_removed';
+
+export default function userReducer(state = {}, action) {
     switch (action.type) {
         case SAVE_USER:
         case REQUEST_USER:
@@ -17,6 +27,69 @@ const userReducer = (state = {}, action) => {
         default:
             return state;
     }
-};
+}
 
-export default userReducer;
+export const requestUser = createAction(REQUEST_USER);
+export const receiveUser = createAction(RECEIVE_USER);
+
+export function fetchUser() {
+    return dispatch => {
+        dispatch(requestUser());
+        return getJson('/api/user')
+            .then(user => dispatch(receiveUser(user)))
+            .catch(err => dispatch(receiveUser(err)));
+    };
+}
+
+export function saveUser(user) {
+    return dispatch => {
+        dispatch({
+            type: SAVE_USER,
+            payload: user,
+        });
+        return putJson(`/api/users/${user._id}`, user)
+            .then(updatedUser => dispatch(receiveUser(updatedUser)));
+    };
+}
+
+export const requestAllUsers = createAction(REQUEST_ALL_USERS);
+export const receiveAllUsers = createAction(RECEIVE_ALL_USERS);
+
+export function fetchAllUsers() {
+    return dispatch => {
+        dispatch(requestAllUsers());
+        return getJson('/api/users')
+            .then(users => dispatch(receiveAllUsers(users)));
+    };
+}
+
+export const requestRoles = createAction(REQUEST_ROLES);
+export const receiveRoles = createAction(RECEIVE_ROLES);
+
+export function fetchRoles() {
+    return dispatch => {
+        dispatch(requestRoles());
+        return getJson('/api/roles')
+            .then(roles => dispatch(receiveRoles(roles)));
+    };
+}
+
+export const roleAdded = createAction(ROLE_ADDED);
+
+export function addRole({ user, userRole }) {
+    return dispatch => {
+        const roles = [userRole].concat(user.roles);
+        putJson(`/api/users/${user._id}`, { roles })
+            .then(updatedUser => dispatch(roleAdded(updatedUser)));
+    };
+}
+
+export const roleRemoved = createAction(ROLE_REMOVED);
+
+export function removeRole(user, roleToRemove) {
+    return dispatch => {
+        const roles = user.roles.filter(role => role !== roleToRemove);
+        putJson(`/api/users/${user._id}`, { roles })
+            .then(updatedUser => dispatch(roleRemoved(updatedUser)));
+    };
+}
