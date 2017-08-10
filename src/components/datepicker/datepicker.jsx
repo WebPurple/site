5453e86232e3ceae711076b9bc3355134ae08c3c
Bobrovskaya, Iana <yana55588@mail.ru>
2017-08-10 12:26:29 +0300

    move react datepicker to the common component, stylizing

---
+++ src/components/datepicker/datepicker.jsx
@@ -0,0 +1,8 @@
+import ReactDatePicker from 'react-datepicker';
+
+import 'react-datepicker/dist/react-datepicker.css';
+import 'react-select/dist/react-select.css';
+import './datepicker.css';
+
+const Datepicker = ReactDatePicker;
+export default Datepicker;
