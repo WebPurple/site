88bb299847133c2a00b3a2997ff5a2ee742e1afd
Savva Kodeikin <savva_kodeikin@epam.com>
2017-10-30 16:22:44 +0300

    🐛 added a fix for padding in the speakers block

--- src/components/event-page/talks/talks.jsx
+++ src/components/event-page/talks/talks.jsx
@@ -9,6 +9,7 @@ const TalksGrid = styled.div`
     column-fill: balance;
     column-gap: 12px;
     margin-top: 6.4rem;
+    margin-bottom: ${ props => props.talksCount % 2 > 0 ? ' 6.4rem' : '0' };
 
     ${media.desktop`
         column-count: 2;
@@ -16,7 +17,7 @@ const TalksGrid = styled.div`
 `;
 
 const EventTalks = ({ talks }) => (
-    <TalksGrid>
+    <TalksGrid talksCount={talks.length}>
         { talks.map(talk => <EventTalk key={talk.title} talk={talk} />) }
     </TalksGrid>
 );
