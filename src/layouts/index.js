9ab95b93a22ca9cb7d3ee8bf63a43346ea46cf8f
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-03-26 23:10:18 +0300

    feat: :sparkles: add webpurple header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import styled, { ThemeProvider } from 'styled-components'
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Header from '../components/header'
+
+const Container = styled.div`
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  ${media.desktop`width: 1280px;`} ${media.hd`width: 1440px;`};
+`
 
 const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
-    <div>{children()}</div>
-  </div>
+  <Container>
+    <Helmet title="WebPurple" />
+    <Header />
+    <main>{children()}</main>
+  </Container>
 )
 
 TemplateWrapper.propTypes = {
