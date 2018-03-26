5b58303d538e2b2b4d076044c9f0d51098a82384
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-03-26 09:38:17 +0300

    :sparkles: setup gatsby

--- e2e/step_definitions/constants.js
+++
@@ -1,2 +0,0 @@
-// eslint-disable-next-line
-export const appUrl = 'http://localhost:4200'
---
+++ src/components/Testimonials.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Testimonials = ({ testimonials }) => (
+  <div>
+    {testimonials.map(testimonial => (
+      <article className="message">
+        <div className="message-body">
+          {testimonial.quote}
+          <br />
+          <cite> – {testimonial.author}</cite>
+        </div>
+      </article>
+    ))}
+  </div>
+)
+
+Testimonials.propTypes = {
+  testimonials: PropTypes.arrayOf(
+    PropTypes.shape({
+      quote: PropTypes.string,
+      author: PropTypes.string,
+    })
+  ),
+}
+
+export default Testimonials
