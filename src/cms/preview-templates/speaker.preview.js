911332c484f23d8a7bb5e283a6ff30b54bef54c2
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-04-08 16:01:51 +0300

    :poop: debug preview in production

--- src/cms/preview-templates/speaker.preview.js
+++ src/cms/preview-templates/speaker.preview.js
@@ -4,6 +4,7 @@ import SpeakerCard from '../../components/speaker-card/speaker-card'
 export default ({ entry }) => (
   <div>
     {JSON.stringify(entry.toJS().data)}
-    <SpeakerCard speaker={entry.toJS().data} />
+    {JSON.stringify(entry.toJS())}
+    {/*<SpeakerCard speaker={entry.toJS().data} />*/}
   </div>
 )
