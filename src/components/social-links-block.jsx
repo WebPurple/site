c7146fe46733ec25c88ada71713da6efb1f7c4ea
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-04-10 23:04:24 +0300

    :sparkles: add yandex metrics goals

--- src/components/social-links-block.jsx
+++ src/components/social-links-block.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Flex, Box } from 'grid-styled'
+import ym from 'react-yandex-metrika'
 
 import { media } from '../utils/css-utils'
 import BlockHeader from './common/block-header'
@@ -26,26 +27,31 @@ let socialNetworks = [
     link: 'https://vk.com/webpurple',
     description: 'We are in Vkontakte',
     icon: responsiveHeight(VkIcon, 22, 42, 50),
+    ymGoal: 'vkontakte',
   },
   {
     link: 'https://www.facebook.com/WebPurple',
     description: 'We are in Facebook',
     icon: responsiveHeight(FacebookSquareIcon, 30, 65, 80),
+    ymGoal: 'facebook',
   },
   {
     link: 'https://www.youtube.com/channel/UCFOQWgbqJbU8sVuWHGln2aA',
     description: 'Our youtube channel',
     icon: responsiveHeight(YoutubeIcon, 30, 60, 70),
+    ymGoal: 'youtube',
   },
   {
     link: 'https://t.me/WebPurple',
     description: 'We are in telegram',
     icon: responsiveHeight(TelegramIcon, 30, 55, 65),
+    ymGoal: 'telegram',
   },
   {
     link: 'https://www.instagram.com/webpurple',
     description: 'We are in instagram',
     icon: responsiveHeight(InstagramIcon, 30, 65, 80),
+    ymGoal: 'instagram',
   },
 ]
 
@@ -58,9 +64,13 @@ export default () => (
       p={0}
       mt={['5rem', '10rem']}
       style={{ listStyle: 'none' }}>
-      {socialNetworks.map(({ link, description, icon: Icon }) => (
+      {socialNetworks.map(({ link, description, icon: Icon, ymGoal }) => (
         <li>
-          <a href={link} target="_blank" rel="noopener noreferrer">
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => ym('reachGoal', ymGoal)}>
             <HiddenText>{description}</HiddenText>
             <Icon />
           </a>
