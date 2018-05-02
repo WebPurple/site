26755006e0df67a979ff1c087a4aa7aff56187a7
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-05-02 15:47:40 +0300

    chore(gatsby): extract events page creation logic to separate function

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,54 +1,47 @@
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
 
-exports.createPages = ({ boundActionCreators, graphql }) => {
-  let { createPage } = boundActionCreators
-
-  return new Promise((resolve, reject) => {
-    let eventTemplate = path.resolve(`src/templates/event.template.js`)
-    resolve(
-      graphql(
-        `
-          {
-            allEventYaml(sort: { fields: [date], order: DESC }) {
-              edges {
-                node {
-                  id
-                  fields {
-                    slug
-                  }
-                  title
-                  description
-                  date
-                  address
-                  talks {
-                    title
-                    speaker
-                  }
-                }
+let createEventPages = ({ boundActionCreators: { createPage }, graphql }) =>
+  graphql(
+    `
+      {
+        allEventYaml(sort: { fields: [date], order: DESC }) {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              title
+              description
+              date
+              address
+              talks {
+                title
+                speaker
               }
             }
           }
-        `,
-      ).then(result => {
-        if (result.errors) {
-          reject(result.errors)
         }
+      }
+    `,
+  ).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors)
+    }
 
-        result.data.allEventYaml.edges.forEach(({ node }) => {
-          createPage({
-            path: node.fields.slug,
-            component: eventTemplate,
-            // layout: `blog-layout`,
-            context: {
-              id: node.id,
-            },
-          })
-        })
-      }),
-    )
+    result.data.allEventYaml.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: path.resolve(`src/templates/event.template.js`),
+        context: {
+          id: node.id,
+        },
+      })
+    })
   })
-}
+
+exports.createPages = (...args) => Promise.all([createEventPages(...args)])
 
 exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
   let { createNodeField } = boundActionCreators
