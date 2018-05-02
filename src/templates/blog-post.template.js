1c77c941a1a946139c051b83881bc6f6a9ab2fd0
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-05-02 22:33:12 +0300

    feat(blog): stylize blog post header

--- src/templates/blog-post.template.js
+++ src/templates/blog-post.template.js
@@ -1,11 +1,55 @@
 import * as React from 'react'
 import { mapProps } from 'recompose'
+import styled from 'styled-components'
+import { Flex, Box } from 'grid-styled'
+import { TagList } from '../components/common/tag'
+
+let Header = styled.header`
+  background-image: linear-gradient(to bottom, #be00ff, #6200ff);
+`
+
+let Heading = styled.h1`
+  font-family: Rubic, sans-serif;
+  color: #fff;
+`
+
+let Avatar = styled.div`
+  background: url(${props => props.background});
+  background-position: center;
+  background-size: cover;
+  height: 6rem;
+  width: 6rem;
+  border-radius: 50%;
+`
+
+let Oxigen = styled.span`
+  font-family: Oxigen, sans-serif;
+`
 
 let BlogPost = ({ post }) => (
   <React.Fragment>
-    <h1>{post.title}</h1>
-    <strong>{post.author} ({new Date(post.date).toLocaleString()})</strong>
-    <div dangerouslySetInnerHTML={{ __html:post.content }} />
+    <Box is={Header} p={['6rem 2rem', '9.6rem 12rem']}>
+      <TagList tags={post.tags} />
+      <Flex alignItems="center" my={['3.2rem', '4.8rem']}>
+        <Avatar background="/img/3a10409e4afd745b080502e5fb10df93.jpg" />
+        <Flex flexDirection="column" color="white" ml="1.6rem">
+          <Box is={Oxigen} fontSize={['1.8rem', '2.4rem']} mb=".8rem">
+            {post.author}
+          </Box>
+          <Box is={Oxigen} fontSize={['1.4rem', '1.6rem']}>
+            {new Date(post.date).toLocaleString()}
+          </Box>
+        </Flex>
+      </Flex>
+      <Box is={Heading} m={0} fontSize={['3.2rem', '7.8rem']}>
+        {post.title}
+      </Box>
+    </Box>
+    <Flex justifyContent="center">
+      <Box w={['100%', '1078px']}>
+        <div dangerouslySetInnerHTML={{ __html: post.content }} />
+      </Box>
+    </Flex>
   </React.Fragment>
 )
 
