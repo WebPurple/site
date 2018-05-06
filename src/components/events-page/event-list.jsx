0d4ddea269ba5bf5666fba9c884912eddf435fc0
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-05-06 19:30:00 +0300

    chore(events): use grid-styled more...

--- src/components/events-page/event-list.jsx
+++ src/components/events-page/event-list.jsx
@@ -5,7 +5,6 @@ import styled, { withTheme } from 'styled-components'
 import { Box } from 'grid-styled'
 import moment from 'moment'
 
-import { media } from '../../utils/css-utils'
 import { TagList } from '../common/tag'
 import { ClockIcon, PlaceholderIcon } from '../icons'
 import { eventSmallBackground, eventTags } from '../../utils/selectors'
@@ -13,11 +12,6 @@ import AdaptiveMasonryList from '../adaptive-masonry-list'
 
 const EventSnippet = styled.li`
   position: relative;
-  width: 100%;
-  ${media.tablet`width: 30rem;`} ${media.hd`width: 35rem;`} padding: 2.5rem;
-  margin-bottom: 2rem;
-  ${media.desktop`margin-bottom: 7rem`};
-  box-sizing: border-box;
   overflow: hidden;
   box-shadow: 0 0 8px 1px #bbb;
 `
@@ -38,8 +32,7 @@ const BackgroundImage = styled.div`
   height: 100%;
   filter: grayscale(100);
   opacity: 0.15;
-  background: url(${props => props.url});
-  background-repeat: no-repeat;
+  background: url(${props => props.url}) no-repeat;
   background-size: cover;
   transform: skew(60deg, 0);
 `
@@ -70,10 +63,6 @@ const TalkList = styled.ul`
   color: #4a4a4a;
 `
 
-const Talk = styled.li`
-  margin: 1.6rem 0;
-`
-
 const EventList = ({ events, theme, onTagClick, selectedTags }) => (
   <Box
     is={AdaptiveMasonryList}
@@ -81,10 +70,16 @@ const EventList = ({ events, theme, onTagClick, selectedTags }) => (
     mx="auto"
     mt={['3.6rem', '3.6rem', '10rem']}>
     {events.map((event, eventIndex) => (
-      <EventSnippet className="e2e-event-card" key={event.title}>
+      <Box
+        is={EventSnippet}
+        key={event.title}
+        w={['100%', '30rem', '30rem', '35rem']}
+        p="2.5rem"
+        mb={['2rem', '2rem', '7rem']}>
         <BackgroundShape>
           <BackgroundImage url={eventSmallBackground()} />
         </BackgroundShape>
+
         <header>
           <Info>
             <ClockIcon style={{ marginRight: '1.6rem' }} />
@@ -92,10 +87,12 @@ const EventList = ({ events, theme, onTagClick, selectedTags }) => (
               {event.date ? moment(event.date).format('LLL') : 'Уточняется'}
             </time>
           </Info>
+
           <Info>
             <PlaceholderIcon style={{ marginRight: '1.6rem' }} />
             <span>{event.address || 'Уточняется'}</span>
           </Info>
+
           <Title
             className="e2e-event-card-title"
             color={eventIndex % 2 ? theme.vividPurpleTwo : theme.lipstick}
@@ -103,15 +100,21 @@ const EventList = ({ events, theme, onTagClick, selectedTags }) => (
             {event.title}
           </Title>
         </header>
+
         <TalkList>
-          {event.talks.map(talk => <Talk key={talk.title}>{talk.title}</Talk>)}
+          {event.talks.map(talk => (
+            <Box key={talk.title} m="1.6rem">
+              {talk.title}
+            </Box>
+          ))}
         </TalkList>
+
         <TagList
           tags={eventTags(event)}
           onTagClick={onTagClick}
           selectedTags={selectedTags}
         />
-      </EventSnippet>
+      </Box>
     ))}
   </Box>
 )
