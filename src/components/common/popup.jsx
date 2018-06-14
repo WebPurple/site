565e4de22019bbac2e0a296b1ea9fb49e1df10fe
Ruslan Abramov <r.o.abramov@gmail.com>
2018-06-14 12:58:47 +0300

    refactor: extract popup into separate component

--- src/components/common/popup.jsx
+++ src/components/common/popup.jsx
@@ -1,39 +1,59 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
-import Modal from 'react-modal'
-import styled from 'styled-components'
+import { Portal } from 'react-portal'
+import styled, { injectGlobal } from 'styled-components'
 
 import CloseIcon from '../icons/close-icon'
 
-const customStyles = {
-  overlay: {
-    alignItems: 'center',
-    backgroundColor: 'rgba(94, 94, 94, 0.13)',
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  content: {
-    top: 'auto',
-    left: 'auto',
-    right: 'auto',
-    bottom: 'auto',
-    border: 'none',
-    borderRadius: 'none',
-    maxHeight: '100vh',
-    overflow: 'auto',
-    padding: '0',
-    position: 'static',
-  },
-}
-
-const ModalHeader = styled.header`
-  display: flex;
-  flex-direction: row-reverse;
-  padding: 1.2em;
+injectGlobal`
+  body.popup-opened {
+    overflow: hidden;
+    & > *:not(:last-child) {
+      filter: blur(3px);
+    }
+  } 
 `
 
-const ModalContent = styled.div`
-  padding: 0 4em 4em;
+const ESC_KEY_CODE = 27
+const PopupContainer = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Shadow = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-color: rgba(94, 94, 94, 0.13);
+`
+
+const PopupWindow = styled.div`
+  position: relative;
+  width: 100%;
+  max-width: 75rem;
+
+  &:before {
+    display: block;
+    content: '';
+    width: 100%;
+    padding-top: 56.25%;
+  }
+
+  & > * {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+  }
 `
 
 const IconButton = styled.button`
@@ -41,29 +61,54 @@ const IconButton = styled.button`
   border: none;
   cursor: pointer;
   outline: none;
+  position: absolute;
+  width: auto;
+  height: auto;
+  right: -4rem;
+  top: -2rem;
+  left: auto;
 
   &:hover #closeIcon {
     stroke: #ababab;
   }
 `
 
-const Popup = ({ width, children, onRequestClose, ...rest }) => (
-  <Modal
-    {...rest}
-    onRequestClose={onRequestClose}
-    style={{ ...customStyles, content: { ...customStyles.content, width } }}>
-    <ModalHeader>
-      <IconButton onClick={onRequestClose}>
-        <CloseIcon />
-      </IconButton>
-    </ModalHeader>
-    <ModalContent>{children}</ModalContent>
-  </Modal>
-)
+class Popup extends React.Component {
+  render() {
+    const { children, onRequestClose } = this.props
+    return (
+      <Portal>
+        <PopupContainer>
+          <Shadow onClick={onRequestClose} />
+          <PopupWindow>
+            <IconButton onClick={onRequestClose}>
+              <CloseIcon />
+            </IconButton>
+            {children}
+          </PopupWindow>
+        </PopupContainer>
+      </Portal>
+    )
+  }
+
+  componentDidMount() {
+    document.querySelector('body').classList.add('popup-opened')
+    document.addEventListener('keydown', this.closeOnEscape)
+  }
+  componentWillUnmount() {
+    document.querySelector('body').classList.remove('popup-opened')
+    document.removeEventListener('keydown', this.closeOnEscape)
+  }
+
+  closeOnEscape = event => {
+    if (event.keyCode === ESC_KEY_CODE) {
+      this.props.onRequestClose()
+    }
+  }
+}
 
 export default Popup
 
 Popup.propTypes = {
-  onRequestClose: PropTypes.func,
-  width: PropTypes.number,
+  onRequestClose: PropTypes.func.isRequired,
 }
--- src/components/common/video.jsx
+++ src/components/common/video.jsx
@@ -1,59 +1,14 @@
 import React, { Component } from 'react'
-import { Portal } from 'react-portal'
-import styled from 'styled-components'
 import concat from 'ramda/src/concat'
 import id from 'ramda/src/identity'
 
 import { Media } from '../../utils/css-utils'
 import Loader from './loader'
-
-const ESC_KEY_CODE = 27
-
-const PopupContainer = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
-const Shadow = styled.div`
-  position: absolute;
-  top: 0;
-  right: 0;
-  left: 0;
-  bottom: 0;
-  background-color: rgba(200, 197, 197, 0.56);
-`
-
-const PopupWindow = styled.div`
-  position: relative;
-  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.2);
-  width: 100%;
-  max-width: 75rem;
-
-  &:before {
-    display: block;
-    content: '';
-    width: 100%;
-    padding-top: 56.25%;
-  }
-
-  & > * {
-    position: absolute;
-    left: 0;
-    top: 0;
-    width: 100%;
-    height: 100%;
-  }
-`
+import Popup from './Popup'
 
 const VideoLoader = Loader.extend`
   &:after {
-    background-color: rgb(231, 216, 216);
+    background-color: rgb(229, 212, 212);
   }
 `
 
@@ -70,32 +25,15 @@ class Video extends Component {
    */
   showPopup = event => {
     event.preventDefault()
-    document.querySelector('body').style.overflow = 'hidden'
-    document.addEventListener('keydown', this.closeOnEscape)
     this.setState({ isPlaying: true })
   }
 
   closePopup = () => {
     this.setState(initialState)
-    document.querySelector('body').style.overflow = 'visible'
-    document.removeEventListener('keydown', this.closeOnEscape)
   }
 
   onVideoLoad = () => {
     this.setState({ isLoaded: true })
-  }
-
-  /**
-   * @param {KeyboardEvent} event
-   */
-  closeOnEscape = event => {
-    if (event.keyCode === ESC_KEY_CODE) {
-      this.closePopup()
-    }
-  }
-
-  componentWillUnmount() {
-    document.querySelector('body').style.overflow = 'visible'
   }
 
   setEmebededUrl = concat('https://www.youtube.com/embed/')
@@ -116,22 +54,21 @@ class Video extends Component {
             src: this.setEmebededUrl(this.props.src),
           })}
           {this.state.isPlaying && (
-            <Portal>
-              <PopupContainer>
-                <Shadow onClick={this.closePopup} />
-                {!this.state.isLoaded && <VideoLoader size="80" border="8" />}
-                <PopupWindow
-                  style={{ display: this.state.isLoaded ? 'initial' : 'none' }}>
-                  <iframe
-                    src={this.setEmebededUrl(this.props.src)}
-                    onLoad={this.onVideoLoad}
-                    frameBorder="0"
-                    allow="autoplay; encrypted-media"
-                    allowFullScreen
-                  />
-                </PopupWindow>
-              </PopupContainer>
-            </Portal>
+            <Popup onRequestClose={this.closePopup}>
+              {!this.state.isLoaded && (
+                <div>
+                  <VideoLoader size="80" border="8" />
+                </div>
+              )}
+              <iframe
+                src={this.setEmebededUrl(this.props.src)}
+                onLoad={this.onVideoLoad}
+                style={{ display: this.state.isLoaded ? 'initial' : 'none' }}
+                frameBorder="0"
+                allow="autoplay; encrypted-media"
+                allowFullScreen
+              />
+            </Popup>
           )}
         </Media.TabletPlus>
       </React.Fragment>
