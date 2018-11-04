4afda6720a235418edffbb71d9951af318b646eb
Nikita Kirsanov <kitos.kirsanov@gmail.com>
2018-11-05 00:13:34 +0300

    fix: check if speaker exists

--- src/components/event-page/talks/talk.jsx
+++ src/components/event-page/talks/talk.jsx
@@ -26,60 +26,64 @@ let SpeakerLink = styled(Link)`
   text-decoration: none;
 `
 
-let EventTalk = ({ talk }: { talk: TalkType }) => (
-  <Flex flexDirection={['column', 'row']} alignItems={['center', 'flex-start']}>
-    <Box
-      is={AvatarWrapper}
-      to={talk.speaker.fields.slug.replace('speaker', 'speakers')}
-      mr={['27px', '36px', '24px']}>
-      <Avatar avatar={talk.speaker.avatar} stretch />
-    </Box>
+let EventTalk = ({ talk }: { talk: TalkType }) => {
+  let speakerUrl =
+    talk.speaker && talk.speaker.fields.slug.replace('speaker', 'speakers')
 
-    <div>
-      <Box
-        is={Header}
-        color={tColor('greyishBrown')()}
-        fontSize={['28px', '32px']}
-        m={0}
-        mb="24px">
-        {talk.title}
+  return (
+    <Flex
+      flexDirection={['column', 'row']}
+      alignItems={['center', 'flex-start']}>
+      <Box is={AvatarWrapper} to={speakerUrl} mr={['27px', '36px', '24px']}>
+        <Avatar avatar={talk.speaker.avatar} stretch />
       </Box>
 
-      <Box
-        is={SpeakerLink}
-        to={talk.speaker.fields.slug.replace('speaker', 'speakers')}
-        itemProp="actor"
-        color={tColor('grape')()}
-        fontSize={['16px', '18px']}
-        mb="24px">
-        {talk.speaker.jobTitle
-          ? `${talk.speaker.title}, ${talk.speaker.jobTitle}`
-          : talk.speaker.title}
-      </Box>
-
-      {talk.description && (
+      <div>
         <Box
+          is={Header}
           color={tColor('greyishBrown')()}
+          fontSize={['28px', '32px']}
+          m={0}
+          mb="24px">
+          {talk.title}
+        </Box>
+
+        <Box
+          is={SpeakerLink}
+          to={speakerUrl}
+          itemProp="actor"
+          color={tColor('grape')()}
           fontSize={['16px', '18px']}
           mb="24px">
-          {talk.description}
+          {talk.speaker.jobTitle
+            ? `${talk.speaker.title}, ${talk.speaker.jobTitle}`
+            : talk.speaker.title}
         </Box>
-      )}
 
-      {talk.links && (
-        <Flex justifyContent={['space-around', 'flex-start']}>
-          {talk.links.video && (
-            <Box is={YoutubeLink} url={talk.links.video} mr={[0, '36px']} />
-          )}
-          {talk.links.presentation && (
-            <FileLink icon={DownloadIcon} href={talk.links.presentation}>
-              Presentation
-            </FileLink>
-          )}
-        </Flex>
-      )}
-    </div>
-  </Flex>
-)
+        {talk.description && (
+          <Box
+            color={tColor('greyishBrown')()}
+            fontSize={['16px', '18px']}
+            mb="24px">
+            {talk.description}
+          </Box>
+        )}
+
+        {talk.links && (
+          <Flex justifyContent={['space-around', 'flex-start']}>
+            {talk.links.video && (
+              <Box is={YoutubeLink} url={talk.links.video} mr={[0, '36px']} />
+            )}
+            {talk.links.presentation && (
+              <FileLink icon={DownloadIcon} href={talk.links.presentation}>
+                Presentation
+              </FileLink>
+            )}
+          </Flex>
+        )}
+      </div>
+    </Flex>
+  )
+}
 
 export default EventTalk
